b0458a33d09a207804d7f5f6f3edca5c003ab72d
Alejandro Uscátegui <uscateguit@gmail.com>
2024-02-01 21:09:57 -0500

    First Commit :tada:

---
+++ src/main/java/com/capibaras/bottomline/BottomlineApplication.java
@@ -0,0 +1,13 @@
+package com.capibaras.bottomline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BottomlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capibaras/bottomline/BottomlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capibaras.bottomline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottomlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
