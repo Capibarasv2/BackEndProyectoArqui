5e07f98934728cb8b928d4913447b5c26af4d630
Javi <javisdgv@gmail.com>
2024-04-16 21:48:24 -0500

    Invoice controller hotfix :ambulance:

--- src/main/java/com/capibaras/bottomline/controllers/InvoiceController.java
+++ src/main/java/com/capibaras/bottomline/controllers/InvoiceController.java
@@ -1,4 +1,24 @@
 package com.capibaras.bottomline.controllers;
 
+import com.capibaras.bottomline.models.Employee;
+import com.capibaras.bottomline.models.Invoice;
+import com.capibaras.bottomline.services.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class InvoiceController {
+
+    @Autowired
+    InvoiceService invoiceService;
+
+    @GetMapping("/index")
+    public List<Invoice> index(){
+        return invoiceService.listInvoices();
+    }
+
+
 }
