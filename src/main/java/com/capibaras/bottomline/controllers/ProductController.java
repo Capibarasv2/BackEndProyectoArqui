f323fe1b59fc86ad3ec10ca8cf06a3603ac37714
estudiante <javisdgv@gmail.com>
2024-04-30 10:34:10 -0500

    ProductDTO created :sparkles:

--- src/main/java/com/capibaras/bottomline/controllers/ProductController.java
+++ src/main/java/com/capibaras/bottomline/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package com.capibaras.bottomline.controllers;
 
+import com.capibaras.bottomline.dtos.ProductDTO;
 import com.capibaras.bottomline.models.Product;
 import com.capibaras.bottomline.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,27 +30,15 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public Product create(@RequestBody Product product) {
-        return productService.save(product);
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDTO productDto) {
+        Product createdProduct = productService.createProduct(productDto);
+        return ResponseEntity.ok(createdProduct);
     }
 
     @PutMapping("/update/{id}")
-    public Product update(@PathVariable Long id, @RequestBody Product product) {
-        Optional<Product> existingProductOptional = productService.findById(id);
-        if (existingProductOptional.isPresent()) {
-            Product existingProduct = existingProductOptional.get();
-            existingProduct.setName(product.getName());
-            existingProduct.setDescription(product.getDescription());
-            existingProduct.setCategory(product.getCategory());
-            existingProduct.setSerial_number(product.getSerial_number());
-            existingProduct.setPurchase_price(product.getPurchase_price());
-            existingProduct.setSale_price(product.getSale_price());
-            existingProduct.setQuantity_available(product.getQuantity_available());
-            existingProduct.setProduct_thumbnail(product.getProduct_thumbnail());
-            existingProduct.setProduct_state(product.getProduct_state());
-            return productService.save(existingProduct);
-        }
-        return null;
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDto) {
+        Product updatedProduct = productService.updateProduct(id, productDto);
+        return ResponseEntity.ok(updatedProduct);
     }
 
 
--- src/main/java/com/capibaras/bottomline/services/ProductService.java
+++ src/main/java/com/capibaras/bottomline/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.capibaras.bottomline.services;
 
+import com.capibaras.bottomline.dtos.ProductDTO;
 import com.capibaras.bottomline.models.Product;
+import com.capibaras.bottomline.models.Supplier;
 import com.capibaras.bottomline.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import java.util.Optional;
 public class ProductService {
 
     private ProductRepository productRepository;
+    private SupplierService supplierService;
 
     @Autowired
     public ProductService(ProductRepository productRepository) {
@@ -36,6 +39,36 @@ public class ProductService {
 
     public long count() {
         return productRepository.count();
+    }
+
+    public Product createProduct(ProductDTO productDto) {
+        Product product = new Product();
+        return getProduct(productDto, product);
+    }
+
+    public Product updateProduct(Long id, ProductDTO productDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+        return getProduct(productDto, product);
+    }
+
+    private Product getProduct(ProductDTO productDto, Product product) {
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setCategory(productDto.getCategory());
+        product.setSerial_number(productDto.getSerial_number());
+        product.setPurchase_price(productDto.getPurchase_price());
+        product.setSale_price(productDto.getSale_price());
+        product.setQuantity_available(productDto.getQuantity_available());
+        product.setProduct_thumbnail(productDto.getProduct_thumbnail());
+        product.setProduct_state(productDto.getProduct_state());
+
+        Supplier supplier = supplierService.getSupplierById(productDto.getSupplier_id())
+                .orElseThrow(() -> new RuntimeException("Supplier not found with id: " + productDto.getSupplier_id()));
+        product.setSupplier(supplier);
+
+        return productRepository.save(product);
     }
 
 }
